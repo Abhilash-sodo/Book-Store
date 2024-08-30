67d8f44ad203dc2da37b6d19d28c0f05c927fbeb
Abhilash-sodo <83344705+Abhilash-sodo@users.noreply.github.com>
2024-08-30 11:34:19 +0530

    Add Backend directory

---
+++ Backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+export const mongoDBURL =
+'mongodb+srv://workwithus787:x3uVxDvEP2TSx8am@book-store-mern.s7oy8.mongodb.net/?retryWrites=true&w=majority&appName=book-store-mern'
